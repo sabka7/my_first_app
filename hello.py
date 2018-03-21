e7d6a9a8771a9d177a2750c5efa8f9c6b6c03687
Saba Alam <sabaalam@Sabas-MacBook-Air.local>
2018-03-21 20:39:28 +0000

    My first app

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("MyApp") #all @... after this line must be @app because that is how you defined stuff at the start
 
@@ -13,6 +13,16 @@ def idontexist():
 @app.route("/saba")
 def saba():
     return "affogato"
+
+@app.route("/<name>")
+def hello_someone(name):
+    return render_template("hello.html", name=name.title())
+
+@app.route("/signup", methods=["POST"]) # post method is necessary because it is recieving data
+def sign_up():
+    form_data = request.form
+    print form_data["email"] #prints the email in the terminal
+    return "All OK"
 
 app.run(debug=True) # runs the application, at this point if nothing else is written, it is waiting for a web browser
 # can also use localhost:5000 to run on browser
